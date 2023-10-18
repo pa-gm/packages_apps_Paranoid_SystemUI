2527a5d3404134bc4501d019ef03e45c976a15f4
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-18 06:25:13 +0530

    ParanoidSystemUI: Introduce smooth display QS tile
    AKA refresh rate tile.
    Thanks to WSTxda for the icon.
    handleClick() logic inspired by Caffeine tile.
    
    Change-Id: If95d751e01ad77b8afef6a16dce3c933cc95310e

--- src/co/aospa/systemui/qs/tileimpl/ParanoidQSModule.kt
+++ src/co/aospa/systemui/qs/tileimpl/ParanoidQSModule.kt
@@ -26,6 +26,7 @@ import co.aospa.systemui.qs.tiles.DataSwitchTile;
 import co.aospa.systemui.qs.tiles.DcDimmingTile;
 import co.aospa.systemui.qs.tiles.HeadsUpTile;
 import co.aospa.systemui.qs.tiles.PowerShareTile;
+import co.aospa.systemui.qs.tiles.SmoothDisplayTile;
 import co.aospa.systemui.qs.tiles.WifiTile
 import dagger.Binds
 import dagger.Module
@@ -82,6 +83,12 @@ interface ParanoidQSModule {
     @IntoMap
     @StringKey(PowerShareTile.TILE_SPEC)
     fun bindPowerShareTile(powerShareTile: PowerShareTile): QSTileImpl<*>
+
+    /** Inject SmoothDisplayTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SmoothDisplayTile.TILE_SPEC)
+    fun bindSmoothDisplayTile(smoothDisplayTile: SmoothDisplayTile): QSTileImpl<*>
 
     /** Inject WifiTile into tileMap in QSModule */
     @Binds
